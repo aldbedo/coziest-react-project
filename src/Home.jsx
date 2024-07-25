9310bbd73f575db1086751a7b774519a6eb85702
aldbedo <jansuguitan@gmail.com>
2024-07-25 19:56:18 +0800

    initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import Footer from "./sections/Footer";
+import Header from "./sections/Header";
+
+function Home() {
+
+    return(
+        <div>
+            <div><Header /></div>
+            <div className="app-footer-container"><Footer /></div>
+        </div>
+    );
+}
+
+export default Home
