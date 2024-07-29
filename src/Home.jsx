a70e5cb5aa1da6ea94f97fd7cf9fb357a4d2b178
aldbedo <jansuguitan@gmail.com>
2024-07-29 17:05:08 +0800

    edit carousel

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,20 +3,14 @@ import Header from "./sections/Header";
 import AppCarousel from "./sections/AppCarousel";
 
 function Home() {
-
-    const images = [
-        'src/assets/banner1.jpg',
-        'src/assets/banner2.jpg',
-        'src/assets/banner3.jpg',
-    ];
     
     return (
         <div className="wrapper">
             <header>
                 <Header />
             </header>
-            <section>
-                <AppCarousel images={images} />
+            <section className="carousel">
+                <AppCarousel />
             </section>
             <footer className="app-footer-container">
                 <Footer />
--- src/sections/AppCarousel.jsx
+++ src/sections/AppCarousel.jsx
@@ -1,110 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
-function AppCarousel({ images }) {
-
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [slideDirection, setSlideDirection] = useState('');
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCurrentIndex((currentIndex + 1) % images.length);
-        }, 3000);
-        return () => clearInterval(intervalId);
-    }, [currentIndex, images]);
-
-    const nextSlide = () => {
-        setSlideDirection('next');
-        setCurrentIndex((currentIndex + 1) % images.length);
-    };
-
-    const prevSlide = () => {
-        setSlideDirection('prev');
-        setCurrentIndex((currentIndex - 1 + images.length) % images.length);
-    };
-
-    // const firstSlide = () => {
-    //     setSlideDirection('next');
-    //     setCurrentIndex(0);
-    // }
-
-    // const secondSlide = () => {
-    //     setSlideDirection('next');
-    //     setCurrentIndex(1);
-    // }
-
-    // const thirdSlide = () => {
-    //     setSlideDirection('next');
-    //     setCurrentIndex(2);
-    // }
-
-    const handleIndicatorClick = (index) => {
-        setSlideDirection('next');
-        setCurrentIndex(index);
-    };
-
-
+function AppCarousel() {
     return (
-        <div className="carousel-container">
-            <div className="carousel-indicators">
-                {images.map((image, index) => (
-                    <button
-                        key={index}
-                        type="button" 
-                        className={`indicator-button ${currentIndex === index ? 'active' : ''}`} 
-                        onClick={() => handleIndicatorClick(index)}
-                />
-                ))}
-            </div>
-            <div>
-                {images.map((image, index) => (
-                    <img
-                        key={index}
-                        src={image}
-                        alt="carousel"
-                        className={`carousel-img ${index === currentIndex ? 'active' : ''} ${slideDirection === 'next' ? 'next-slide' : 'prev-slide'}`}
-                    />
-                ))}
-            </div>
-            <button
-                className="carousel-button prev"
-                onClick={prevSlide}>
-                    &#10094;
-                </button>
-            <button
-                className="carousel-button prev"
-                onClick={nextSlide}>
-                    &#10095;
-                </button>
-                {/* <button 
-                    type="button" 
-                    className={`indicator-button ${currentIndex === 1 ? 'active' : ''}`} 
-                    onClick={secondSlide}
-                />
-                <button 
-                    type="button" 
-                    className={`indicator-button ${currentIndex === 2 ? 'active' : ''}`} 
-                    onClick={thirdSlide}
-                />
-            <div>
-                {images.map((image, index) => (
-                    <img
-                    key={index}    
-                    src={images[currentIndex]} 
-                    alt="carousel" 
-                    className={`carousel-img ${index === currentIndex ? 'active' : ''} ${slideDirection === 'next' ? 'next-slide' : 'prev-slide'}`}
-                />
-                ))}
-            </div>
-            <button 
-                    className="carousel-button prev"
-                    onClick={prevSlide}>&#10094;
-            </button>
-            <button 
-                className="carousel-button next" 
-                onClick={nextSlide}>&#10095;
-            </button> */}
-        </div>
+    <Carousel>
+        <Carousel.Item>
+            <img className="d-block w-100" src="./src/assets/banner1.jpg" alt="FIrst Slide"/>
+            <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img className="d-block w-100" src="./src/assets/banner2.jpg" alt="Second Slide"/>
+            <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img className="d-block w-100" src="./src/assets/banner3.jpg" alt="Second Slide"/>
+            <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                </p>
+            </Carousel.Caption>
+        </Carousel.Item>
+    </Carousel>
     );
 }
 
-export default AppCarousel
+export default AppCarousel;
